a17611608c74e12100d329fa6f5ff0d6b6dbb11c
Nicole Valentini <ncvalentini@gmail.com>
2021-11-09 19:43:21 -0700

    Add comments to exercise 1

--- exercise-1/addNumbers.js
+++ exercise-1/addNumbers.js
@@ -8,12 +8,14 @@ function addNumbers() {
   var nums = [firstNum, secondNum]
 
   var product = firstNum * secondNum
+  // declare new variable called average, adding the variable firstNum
   var sum = firstNum + secondNum
-  var average = sum / nums.length
 
+  var average = sum / nums.length
+//log to the console idex 1 of array numbs. Expected.
   console.log(nums[0])
   console.log(nums[1])
-
+// Log to the console the string ' The first number is 3 '
   console.log(`The first number is ${num1} and the second number is ${num2}!`)
 
   return sum
